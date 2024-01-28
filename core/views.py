f66f379c137e076bf36412a61c1e0f2fb15a6667
unknown <natnaelm552@gmail.com>
2024-01-28 21:01:01 +0300

    debug7

--- core/views.py
+++ core/views.py
@@ -93,7 +93,7 @@ def download_sugar_csv(request):
                 "Content-Disposition" : 'attachment; filename="sugar_stock.csv"'
             }    )
     writer = csv.writer(response)
-    data = models.Stock.objects.all()
+    data = models.Stock.objects.values()
     serializerSugar = serializers.GoodSerializer(data = data, many=True, context= {'item': 'S', 'unit': settings.SUGAR_UNIT})
     try:
         serializerSugar.is_valid(raise_exception=True)
@@ -116,7 +116,7 @@ def download_oil_csv(request):
                 "Content-Disposition" : 'attachment; filename="oil_stock.csv"'
             }    )
     writer = csv.writer(response)
-    data = models.Stock.objects.all()
+    data = models.Stock.objects.values()
     serializerOil = serializers.GoodSerializer(data = data, many=True, context= {'item': 'O', 'unit': settings.OIL_UNIT})
     try:
         serializerOil.is_valid(raise_exception=True)
